82ccdb13c53b61f0c5383e6363844ec36adf3cf9
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-05-14 08:08:46 +0700

    fix navbar: logo href

--- frontend/src/pages/landing/Navbar.jsx
+++ frontend/src/pages/landing/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ position }) => {
             <Toaster />
             <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
+                    <a href="/" className="-m-1.5 p-1.5">
                         <span className="sr-only">Yayasan Annur Rahman Rahim</span>
                         <img
                             alt="logo yayasan annur rahman rahim"

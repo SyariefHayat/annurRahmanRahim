051f444f6d698b19ee6fc7e298545651649cdeed
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-07-10 16:49:57 +0700

    update frontend: article background and border

--- frontend/src/components/modules/article/ArticleHeader.jsx
+++ frontend/src/components/modules/article/ArticleHeader.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 
-import ClipPathUp from '../element/ClipPath/ClipPathUp'
-import ClipPathDown from '../element/ClipPath/ClipPathDown'
-
 const ArticleHeader = () => {
     return (
-        <header className="relative w-full h-screen flex items-center justify-center">
-            <ClipPathUp />
-            <div className="mx-auto max-w-3xl mt-20 px-6 sm:px-8 text-center">
-                <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Blog Annur Rahman Rahim</h1>
-                <p className="mt-8 text-lg font-medium text-gray-500 sm:text-xl">Temukan cerita inspiratif, pembaruan aktivitas yayasan, artikel edukatif, dan banyak lagi di sini.</p>
+        <section aria-label="Program Hero" className="relative w-full h-screen flex items-center justify-center bg-[url(/arah-tujuan.webp)] bg-cover bg-center text-white">
+            <div className="absolute inset-0 bg-black/50 z-0"></div>
+            <div className="relative mx-auto max-w-3xl mt-20 px-6 sm:px-8 text-center">
+                <h1 className="text-5xl font-semibold tracking-tight sm:text-7xl">Blog Annur Rahman Rahim</h1>
+                <p className="mt-8 text-lg font-medium sm:text-xl">Temukan cerita inspiratif, pembaruan aktivitas yayasan, artikel edukatif, dan banyak lagi di sini.</p>
             </div>
-            <ClipPathDown />
-        </header>
+        </section>
     )
 }
 
--- frontend/src/components/modules/article/ArticleList.jsx
+++ frontend/src/components/modules/article/ArticleList.jsx
@@ -7,7 +7,7 @@ import ArticleCardSkeleton from './ArticleCardSkeleton'
 const ArticleList = ({ loading, articleData }) => {
     return (
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto my-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 border-t border-gray-300 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div className="mx-auto my-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 sm:mt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                 {!loading ? (
                     <EachUtils
                         of={articleData}

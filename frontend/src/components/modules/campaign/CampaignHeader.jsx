b76e3b700fb139cacd445ede569d4c331c856abe
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-07-10 16:37:46 +0700

    update frontend: campaign page background

--- frontend/src/components/modules/campaign/CampaignHeader.jsx
+++ frontend/src/components/modules/campaign/CampaignHeader.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
-import ClipPathUp from '@/components/modules/element/ClipPath/ClipPathUp';
-import ClipPathDown from '@/components/modules/element/ClipPath/ClipPathDown';
-
 export const CampaignHeader = () => {
     return (
-        <header className="relative w-full h-screen flex items-center justify-center">
-            <ClipPathUp />
-            <div className="mx-auto max-w-3xl mt-20 px-6 sm:px-8 text-center">
-                <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+        <section aria-label="Program Hero" className="relative w-full h-screen flex items-center justify-center bg-[url(/1.webp)] bg-cover bg-center text-white">
+            <div className="absolute inset-0 bg-black/50 z-0"></div>
+            <div className="relative mx-auto max-w-3xl mt-20 px-6 sm:px-8 text-center">
+                <h1 className="text-5xl font-semibold tracking-tight sm:text-7xl">
                     Jadilah Bagian dari Perubahan
                 </h1>
-                <p className="mt-8 text-lg font-medium text-gray-500 sm:text-xl">
+                <p className="mt-8 text-lg font-medium sm:text-xl">
                     Bantu kami menciptakan dunia yang lebih baik dengan memberikan donasi Anda. 
                     Setiap kontribusi, sekecil apa pun, memiliki dampak besar bagi mereka yang membutuhkan.
                 </p>
             </div>
-            <ClipPathDown />
-        </header>
+        </section>
     );
 };
--- frontend/src/components/modules/campaign/CampaignList.jsx
+++ frontend/src/components/modules/campaign/CampaignList.jsx
@@ -7,7 +7,7 @@ import { CampaignCardSkeleton } from './CampaignCardSkeleton';
 export const CampaignList = ({ loading, campaignData }) => {
     return (
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto my-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 border-t border-gray-300 pt-10 sm:my-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div className="mx-auto my-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 sm:my-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                 {!loading ? (
                     <EachUtils
                         of={campaignData}

b76c14bfa9ba250a5761e0ab714368842099289a
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-07-10 14:20:14 +0700

    update frontend: faq section mobile gap

--- frontend/src/pages/landing/FaqSection.jsx
+++ frontend/src/pages/landing/FaqSection.jsx
@@ -14,7 +14,7 @@ import SectionLayout from '@/components/layouts/SectionLayout'
 const FaqSection = () => {
     return (
         <SectionLayout label="Faq Section">
-            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-center text-gray-900 sm:text-5xl mb-14">Tanya Jawab Umum</h2>
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-center text-gray-900 sm:text-5xl mb-10 sm:mb-14">Tanya Jawab Umum</h2>
 
             <Accordion type="single" collapsible className="w-full">
                 <EachUtils

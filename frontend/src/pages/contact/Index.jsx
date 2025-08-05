ca060624800cdf9d3506444d9b55c0fdadf9f316
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-08-05 12:31:31 +0700

    update frontend: contact header background

--- frontend/src/pages/contact/Index.jsx
+++ frontend/src/pages/contact/Index.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     return (
         <DefaultLayout>
             <Navbar />
-            <div className="relative w-full h-[500px] flex items-center justify-center bg-[url(/value-action.webp)] bg-cover bg-center text-white">
+            <div className="relative w-full h-[500px] flex items-center justify-center bg-[url(/8.webp)] bg-cover bg-center text-white">
                 <div className="absolute inset-0 bg-black/50 z-0"></div>
                 <div className="relative mx-auto max-w-3xl mt-20 px-3 sm:px-0 text-center">
                     <h1 className="text-5xl font-semibold tracking-tight sm:text-7xl">Hubungi Kami</h1>
